a0ebce70065cb3fa694a1f881ffb2d48315c2bc6
Ivan <tymkiv.iv@gmail.com>
2026-06-27 14:49:17 +0300

    hw6.1

---
+++ lesson6/hw6.1.py
@@ -0,0 +1,23 @@
+import string
+
+
+def letters_range(user_input: str):
+    # Розділяємо рядок на дві літери
+    start, end = user_input.split('-')
+
+    # Беремо весь набір літер (латиниця, великі та малі)
+    letters = string.ascii_letters
+
+    # Знаходимо індекси початкової та кінцевої літери
+    start_index = letters.index(start)
+    end_index = letters.index(end)
+
+    # Формуємо підрядок від start до end включно
+    return letters[start_index:end_index + 1]
+
+
+# Приклади використання:
+print(letters_range("I-w"))  # abc d → "abcd"
+print(letters_range("a-w"))  # xyz → "xyz"
+print(letters_range("A-d"))  # ABCD → "ABCD"
+print(letters_range("c-h"))  # cdefgh → "cdefgh"
