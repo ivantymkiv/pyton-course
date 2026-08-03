090a084294dd622b9478ea8a6bac16343eddeceb
Ivan <tymkiv.iv@gmail.com>
2026-08-03 18:21:13 +0300

    hw12.3.py

---
+++ lesson12/hw12.3.py
@@ -0,0 +1,10 @@
+
+def is_even(number: int) -> bool:
+    """
+    Повертає True, якщо число парне,
+    і False, якщо число непарне.
+    Без використання ділення.
+    """
+    return (number & 1) == 0
+print (is_even(2494563894038**2))
+print (is_even(1056897**2))
